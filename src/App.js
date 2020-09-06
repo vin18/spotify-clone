4566b99eb45ff8f76913e39e44b8f58ffa7641e6
Vinit <vinit.coder@gmail.com>
2020-09-06 20:47:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Hellooo React!</h1>
+    </div>
+  );
+};
+
+export default App;
